99c19e09b3ecdcc940480cfa01f54120a0d0af9d
Professor <155454115+TrashDono@users.noreply.github.com>
2024-04-23 03:49:23 +0800

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+os.system('apt install npm')
+os.system('npm install https')
+os.system('pkg install qb64')
+os.system('pkg install openjdk-17')
+os.system('pkg install nodejs-lts')
+os.system('npm install url')
+os.system('npm install net')
+os.system('npm install crypto')
+os.system('npm install axios')
+os.system('npm install request')
+os.system('npm install header-generator')
+os.system('node Medusa.js')
+print(f"installation successful")
