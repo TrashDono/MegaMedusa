6a8e35ecee1d26f37f56526c272cf0060f6341db
Professor <155454115+TrashDono@users.noreply.github.com>
2024-10-06 06:27:11 +0800

    Update nvminstall.py

--- nvminstall.py
+++ nvminstall.py
@@ -1,12 +1,9 @@
 import os
 import sys
 
-
-os.system('pkg update -y && pkg install wget curl proot tar -y && wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Debian/debian.sh -O debian.sh && chmod +x debian.sh && bash debian.sh')
 os.system('apt install npm')
 os.system('sudo apt install curl ')
 os.system('curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash')
-os.system('source ~/.bashrc')
 os.system('nvm install --lts')
 os.system('source ~/.bashrc')
 
